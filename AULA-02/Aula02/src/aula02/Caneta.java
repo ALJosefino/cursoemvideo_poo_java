f6598927ad1b437a9bad8a883d11f22cdd576792
André L Josefino <ALJosefino@gmail.com>
2023-11-26 22:55:19 -0300

    Criações
    Criado repositório do curso
    Criadas subpastas da aula 01, 02, e 03
    Colocados arquivos de estudos do curso sobre as aulas01 e 02
    
    Criadas Classes Caneta e Aula02

---
+++ AULA-02/Aula02/src/aula02/Caneta.java
@@ -0,0 +1,38 @@
+
+package aula02;
+
+public class Caneta {
+    
+// Atributos
+
+    String modelo;
+    
+    String cor;
+    
+    float ponta;
+    
+    int carga;
+    
+    boolean tampa;
+
+// Métodos
+    
+    void rabiscar() {
+    
+    
+        
+    }
+    
+    void tampar() {
+    
+    
+    
+    }
+    
+    void destampar() {
+    
+    
+    
+    }
+        
+}
