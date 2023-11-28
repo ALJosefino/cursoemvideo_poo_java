c19c841b110a14c39b6df578a0a6a8aedfb76bde
André L Josefino <ALJosefino@gmail.com>
2023-11-27 22:06:01 -0300

    Classe Caneta e definição dos métodos
    Deletados comentários inseridos automaticamente.
    Classe Caneta e definição dos métodos. instaciada Classe Caneta para teste.

--- AULA03/Aula03/src/aula03/Caneta.java
+++ AULA03/Aula03/src/aula03/Caneta.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package aula03;
 
-/**
- *
- * @author andre
- */
 public class Caneta {
+
+        String modelo;
+        
+        String cor;
+        
+        float ponta;
+        
+        int carga;
+        
+        boolean tampada;
+        
+        void status() {
+        
+            System.out.println( "Caneta modelo: " + this.modelo );
+            
+            System.out.println( "Caneta cor: " + this.cor );
+            
+            System.out.println( "Caneta ponta: " + this.ponta );
+            
+            System.out.println( "Caneta com carga: " + this.carga );
+        
+            System.out.println( "Caneta esta tampada? " + this.tampada );
+            
+        }
     
+        void rabiscar() {
+        
+            if ( this.tampada == true ) {
+            
+                System.out.println("ERRO! Não pode rabiscar, esta tampada. ");
+            
+            } else {
+            
+                System.out.println( "Esta rabiscando! " );
+                
+            }
+                    
+        }
+        
+        void tampar() {
+                    
+            this.tampada = true;
+            
+        }
+
+        void destammpar() {
+
+            this.tampada = false;
+        
+        }
+
 }
