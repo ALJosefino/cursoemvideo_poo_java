2bb07e12b62732f12f50763e4692df0988478dfa
André L Josefino <ALJosefino@gmail.com>
2023-11-30 00:45:37 -0300

    Adicionados
    Atributos, métodos acessores, modificadores e construtor

--- AULA04/Aula04/src/aula04/Aula04.java
+++ AULA04/Aula04/src/aula04/Aula04.java
@@ -4,30 +4,48 @@ package aula04;
 public class Aula04 {
 
     public static void main(String[] args) {
-    
+     
         // INSTANCIANDO DA CLASSE CANETA
+        
+        /*
         
         Caneta c1 = new Caneta();
         
         // ACESSO AOS MÉTODOS MODIFICADORES DA CLASSE DO OBJETO
         
+                
         c1.setModelo("BIC");
         
         //c1.modelo = "BIC";
         
         c1.setPonta(0.5f);
         
-        //c1.ponta = 0.5f;
+        // c1.ponta = 0.5f;
         
         // ACESSO AOS MÉTODOS
         
-        //c1.status();
+        // c1.status();
     
         // USO DE MÉTODOS ACESSORES
         
         System.out.println("Esta é uma caneta MODELO: " + c1.getModelo() +
-                ", com PONTA: " + c1.getPonta() + ".");
+                ", com PONTA: " + c1.getPonta() + "."); 
         
+        */        
+        
+        // INSTANCIANDO DA CLASSE CANETA SEGUNDA VEZ
+        
+        Caneta c2 = new Caneta();
+        
+        c2.status();
+    
+        Caneta c3 = new Caneta();
+        
+        c3.status();
+        
+        Caneta c4 = new Caneta();
+        
+        c4.status();
     }
     
 }
--- AULA04/Aula04/src/aula04/Caneta.java
+++ AULA04/Aula04/src/aula04/Caneta.java
@@ -3,9 +3,35 @@ package aula04;
 
 public class Caneta {
 
+    // DECLARAÇÃO DE ATRIBUTOS COM VISIBILIDADES DE ACESSO
+    
     private String modelo;
     
     private float ponta;
+    
+    private String cor;
+    
+    private boolean tampada;
+    
+    // MÉTODO CONSTRUTOR
+    
+    public Caneta() {
+                
+        this.setTampada(true);
+        
+        // this.tampada = true;
+                
+        this.setCor("Azul");        
+            
+        // this.cor = "Azul";
+        
+        this.setPonta(0.4f);
+        
+        // this.ponta = 0.4f; 
+    
+    }
+        
+    // MÉTODOS ACESSORES E MODIFICADORES
     
     public String getModelo() {
     
@@ -31,6 +57,45 @@ public class Caneta {
     
     }
     
+    
+    public String getCor() {
+    
+        return this.cor;
+    
+    }
+        
+    public void setCor( String c ) {
+    
+        this.cor = c;
+    
+    }
+    
+    public boolean getTampada() {
+    
+        return this.tampada;
+    
+    }
+    
+    public void setTampada( boolean t ) {
+    
+        this.tampada = t;
+    
+    }
+        
+    // OUTROS MÉTODOS
+    
+    public void tampar() {
+    
+        this.setTampada(true);
+    
+    }
+    
+    public void destampar() {
+    
+        this.setTampada(false);
+    
+    }
+        
     public void status() {
     
         System.out.println("SOBRE ESTA CANETA: ");
@@ -39,6 +104,12 @@ public class Caneta {
      
         System.out.println("Caneta com PONTA: " + this.getPonta() );
         
+        System.out.println("Caneta COR: " + this.getCor() );
+        
+        System.out.println("Caneta TAMPADA?: " + this.getTampada() );
+        
+        System.out.println("_______________________________________________");
+                
     }
         
 }
